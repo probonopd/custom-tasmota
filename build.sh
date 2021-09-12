0bf1a73af03afbdfce54d494c2c7faa1ec23c0b1
probonopd <probonopd@users.noreply.github.com>
2021-09-12 21:30:52 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-CONFIGS=$(find configs/ -type f -maxdepth 1)
+CONFIGS=$(cd custom-tasmota ; find configs/ -type f -maxdepth 1)
 for CONFIG in $CONFIGS ; do
   echo $CONFIG
   NAME=$(echo $CONFIG | cut -d '/' -f 2)
   echo "Run PlatformIO for $NAME"
-  cp "$CONFIG" ./tasmota/user_config_override.h
+  cp custom-tasmota/"$CONFIG" ./tasmota/user_config_override.h
   platformio run -e tasmota
   mkdir -p ./out
   mv ./build_output/firmware/tasmota.bin.gz out/tasmota-$NAME-$(git rev-parse --short HEAD).bin.gz
